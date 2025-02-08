f97dd36d6d2183ea5daacb4a83fa0bb8d25aa070
Joseph Banda <jbanda@grey-fox.net>
2025-02-08 12:23:01 -0600

    redid logic for auto adding env and date to log string, so no need to add that manually

--- GVUtilities.psm1
+++ GVUtilities.psm1
@@ -58,6 +58,20 @@ function Out-GVLogFile {
         }
     }
     if ($WriteToLog -eq $true) {
+
+        if ($LogType.toLower() -eq "info") {
+            $LogString = "$(get-date) Info: $($LogString)"
+        }
+        if ($LogType.toLower() -eq "warning") {
+            $LogString = "$(get-date) Warning: $($LogString)"
+        }
+        if ($LogType.toLower() -eq "error") {
+            $LogString = "$(get-date) Error: $($LogString)"
+        }
+        if ($LogType.toLower() -eq "debug") {
+            $LogString = "$(get-date) Debug: $($LogString)"
+        }
+
         $LogFileObject.writeline($LogString)
         $LogFileObject.flush()
     }
